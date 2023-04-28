941022d32678864c0b408910caf58e59cda6e79d
Jianming <Georgegeng00@hotmail.com>
2023-04-27 21:44:19 -0700

    Finish part 2.md

--- expose/javascript/test.js
+++ expose/javascript/test.js
@@ -1,11 +1,34 @@
-function sumValues(num1, num2, add) {
+// Part 1
 
-    if (add) {
-        const result = 0;
-        // result = num1 + num2;
-        console.log('values added: ', result);
-    } else return;
-    console.log('final result: ', result);
+// function sumValues(num1, num2, add) {
+
+//     if (add) {
+//         const result = 0;
+//         // result = num1 + num2;
+//         console.log('values added: ', result);
+//     } else return;
+//     console.log('final result: ', result);
+// }
+
+// sumValues(10, 10, true);
+
+// Part 2
+
+function discountPrices(prices, discount) {
+    const discounted = []; 
+    const length = prices.length;
+
+    for (let i = 0; i < length; i++) {
+        const discountedPrice = prices[i] * (1 - discount);
+        // finalPrice = Math.round(discountedPrice * 100) / 100;
+        discounted.push(discountedPrice);
+    }
+
+    // console.log(i);
+    // console.log(discountedPrice);
+    // console.log(length);
+
+    return discounted;
 }
 
-sumValues(10, 10, true);
+console.log(discountPrices([100, 200, 300], 0.5));
