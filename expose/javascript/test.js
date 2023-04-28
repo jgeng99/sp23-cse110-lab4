8d735cf2e45a0d9f3064f5a830b9f6f512badf2d
Jianming <Georgegeng00@hotmail.com>
2023-04-27 22:18:02 -0700

    Test code

--- expose/javascript/test.js
+++ expose/javascript/test.js
@@ -14,21 +14,35 @@
 
 // Part 2
 
-function discountPrices(prices, discount) {
-    const discounted = []; 
-    const length = prices.length;
+// function discountPrices(prices, discount) {
+//     const discounted = []; 
+//     const length = prices.length;
 
-    for (let i = 0; i < length; i++) {
-        const discountedPrice = prices[i] * (1 - discount);
-        // finalPrice = Math.round(discountedPrice * 100) / 100;
-        discounted.push(discountedPrice);
-    }
+//     for (let i = 0; i < length; i++) {
+//         const discountedPrice = prices[i] * (1 - discount);
+//         // finalPrice = Math.round(discountedPrice * 100) / 100;
+//         discounted.push(discountedPrice);
+//     }
 
-    // console.log(i);
-    // console.log(discountedPrice);
-    // console.log(length);
+//     // console.log(i);
+//     // console.log(discountedPrice);
+//     // console.log(length);
 
-    return discounted;
-}
+//     return discounted;
+// }
 
-console.log(discountPrices([100, 200, 300], 0.5));
+// console.log(discountPrices([100, 200, 300], 0.5));
+
+let student = {
+    name: 'Sarah',
+    major: 'Computer Science',
+    'Grad Year': '2022',
+    greeting: function() {console.log('Hello!'); },
+    'Favorite Teacher': {
+        name: 'Thomas Powell',
+        course: 'CSE 110'
+    },
+    courseLoad: ['CSE 110', 'CSE 134', 'VIS 41']
+};
+
+console.log(student['name']);
